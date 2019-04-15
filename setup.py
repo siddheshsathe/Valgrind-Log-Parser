e8012c659f7e11db8aff9b1b46bb5f694f796b42
Siddhesh Sathe <siddheshs@nvidia.com>
2019-04-15 23:56:12 +0530

    Add json2table dependancy

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 setup(name='valgrind_parser',
-      version='0.2',
+      version='0.2.1',
       description='The valgrind logs parser. Creates the html report from txt logs.',
       url='https://github.com/siddheshsathe/Valgrind-Log-Parser',
       author='Siddhesh Sathe',
       author_email='siddheshsathe@rediffmail.com',
       licence='GNU General Public License v3.0',
       packages=['valgrind_parser'],
+      install_requires=['json2table'],
       zip_safe=False
 )
